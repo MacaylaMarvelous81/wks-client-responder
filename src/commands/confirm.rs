1053019812206ecf81dda30bb88a1bc101b28776
Jomar Milan <jomarm@jomarm.tech>
2024-02-23 19:33:17 -0800

    Remove extra indentation in multiline strings
    Reformats multiline strings to be on the same indentation level as the current block.

--- src/commands/confirm.rs
+++ src/commands/confirm.rs
@@ -1,10 +1,11 @@
 use email_address::EmailAddress;
 
 fn get_headers(encrypt: bool, sender: &EmailAddress, recipient: &EmailAddress) -> String {
-	let common = format!("From: {}\r\n\
-	                              To: {}\r\n\
-								  Subject: Key publication confirmation\r\n\
-								  Wks-Draft-Version: 3\r\n", sender, recipient);
+	let common = format!("\
+	From: {}\r\n\
+	To: {}\r\n\
+	Subject: Key publication confirmation\r\n\
+	Wks-Draft-Version: 3\r\n", sender, recipient);
 	
 	let variable_headers = match encrypt {
 		true => "Content-Type: multipart/encrypted; protocol=\"application/pgp-encrypted\"\r\n",
@@ -17,11 +18,12 @@ fn get_headers(encrypt: bool, sender: &EmailAddress, recipient: &EmailAddress) -
 pub fn confirm_plaintext(user_email_addr: &EmailAddress, wks_email_addr: &EmailAddress, nonce: &String) -> Result<(), &'static str> {
 	let headers = get_headers(false, user_email_addr, wks_email_addr);
 
-	println!("{}\r\n\
-			  type: confirmation-response\r\n\
-			  sender: {}\r\n\
-			  address: {}\r\n\
-			  nonce: {}", headers, wks_email_addr, user_email_addr, nonce);
+	println!("\
+	{}\r\n\
+	type: confirmation-response\r\n\
+	sender: {}\r\n\
+	address: {}\r\n\
+	nonce: {}", headers, wks_email_addr, user_email_addr, nonce);
 	Ok(())
 }
 
